0c60ca3d13fac5349bad24e39ea2f51623f0e1b8
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-12-27 09:38:19 +0100

    Core: code refactor

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-require("dotenv").config()
 const http = require("http")
 const app = require("./app")
-
+require("dotenv").config()
 const server = http.createServer(app)
 const port = process.env.PORT || 3000
 //start server
