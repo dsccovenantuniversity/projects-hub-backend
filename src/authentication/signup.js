0b0a8c36302c4fce86c06b8baeda10dd9ef428a9
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-12-27 17:41:17 +0100

    initi signup.js

---
+++ src/authentication/signup.js
@@ -0,0 +1,7 @@
+const signupAuth = async(req, res)=>{
+    // otp
+    //store send then redirect from frontend
+    if(req.body.username && req.body.email && req.body.password){
+        
+    }
+}
