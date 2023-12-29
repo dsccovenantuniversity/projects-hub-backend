28983e921e3b7803dec9a85fa04857b93a22172d
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-12-29 15:36:50 +0100

    fix: this time, the userdata in entered into the function

--- src/controller/user.js
+++ src/controller/user.js
@@ -4,15 +4,7 @@ const createUser = async (userData) => {
   //receives an object data
   try {
     await prisma.user.create({
-      data: {
-        first_name: "Oluwademiladeogo",
-        last_name: "Bickersteth",
-        email: "bickerstethdemilade@gmail.com",
-        username: "Oluwademiladeogo",
-        google_provider_id: "nil",
-        profile_image: "nil",
-        password: "Oluwademilade",
-      },
+      data: userData,
     });
     return("user created successfully")
   } catch (error) {
