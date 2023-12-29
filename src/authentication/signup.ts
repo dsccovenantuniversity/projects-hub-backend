600019a14650dd9df54c1b1eb852ae0b41a601f6
daudujohn <daudujoe@gmail.com>
2023-12-29 16:19:28 +0100

    feat: convert to typescript

---
+++ src/authentication/signup.ts
@@ -0,0 +1,6 @@
+import { Request, Response } from 'express';
+
+const signupAuth = async (req: Request, res: Response) => {
+    if (req.body.username && req.body.email && req.body.password) {
+    }
+};
