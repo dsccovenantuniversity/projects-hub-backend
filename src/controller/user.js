68b104babfbb5989beb8df2b35641f33cb5ef534
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2023-12-29 15:53:08 +0100

    Feat: added finduserbyid

--- src/controller/user.js
+++ src/controller/user.js
@@ -6,21 +6,34 @@ const createUser = async (userData) => {
     await prisma.user.create({
       data: userData,
     });
-    return("user created successfully")
+    return "user created successfully";
   } catch (error) {
-    return(error);
+    return error;
   }
 };
 
-const findUserbyMail = async(email) => {
-    try {
-        const user = await prisma.user.findUnique({
-            where: {
-              email: email,
-            },
-          });
-          return user
-    } catch (error) {
-        return error
-    }
-  };
+const findUserbyMail = async (email) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
+
+const findUserbyId = async (id) => {
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        google_provider_id: id,
+      },
+    });
+    return user;
+  } catch (error) {
+    return error;
+  }
+};
