8b750410ddd126623d10ca6e997c644d4844433e
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-02 11:01:28 +0100

    Chore: formatting

--- src/controller/dtos/comment.dto.ts
+++ src/controller/dtos/comment.dto.ts
@@ -1,9 +1,9 @@
 export interface createCommentDto {
-    userId: number,
-    projectId: number,
-    body: string
+    userId: number;
+    projectId: number;
+    body: string;
 }
 export interface updateCommentByIdDto {
-    commentId: number, 
-    body: string
+    commentId: number;
+    body: string;
 }
