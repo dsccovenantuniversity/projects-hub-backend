ae60571860c7348565e2cf02c1f9d844ec294f10
daudujohn <daudujoe@gmail.com>
2024-01-08 12:44:35 +0100

    fix: add ssl option for prod environment

--- app.ts
+++ app.ts
@@ -5,21 +5,20 @@ import './src/config/passport_google_oauth2';
 import logger from 'morgan';
 import signupRouter from './src/routes/signup';
 import projectRouter from './src/routes/project';
-import connectPgSimple from 'connect-pg-simple';
-
+import { sessionStore } from './src/config/session-store';
 const app = express();
-const pgSession = connectPgSimple(session);
 
 app.use(express.json());
 
 app.set('trust proxy', 1);
+
 app.use(
     session({
         secret: process.env.SESSION_SECRET ?? 'default_secret',
         resave: false,
         saveUninitialized: true,
         cookie: { secure: false, maxAge: 24 * 60 * 60 * 1000 },
-        store: new pgSession({ createTableIfMissing: true }),
+        store: sessionStore,
     }),
 );
 
