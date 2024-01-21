283c6c8ffb028a07759b5d4d044a4697a82ca254
Oluwademiladeogo <bickersteth.oluwademiladeogo@stu.cu.edu.ng>
2024-01-21 16:34:56 +0100

    Repositories: add deleteVote

--- src/repositories/vote.ts
+++ src/repositories/vote.ts
@@ -1,30 +1,38 @@
-import { prisma } from "../config/prisma";
-import { createVoteDto } from "../interfaces/vote.dto";
+import { prisma } from '../config/prisma';
+import { createVoteDto } from '../interfaces/vote.dto';
 
 export const createVote = async (data: createVoteDto) => {
-        const vote = await prisma.votes.create({
-            data: {
-                user: {
-                    connect: { id: data.userId },
-                },
-                project: {
-                    connect: { id: data.projectId },
-                },
-                up: data.status,
+    const vote = await prisma.votes.create({
+        data: {
+            user: {
+                connect: { id: data.userId },
             },
-        });
-        return vote;
-    } 
-    export const updateVote = async (id: number, status: boolean) => {
-            const currentDate = new Date();
-            const vote = await prisma.votes.update({
-                where: {
-                    id: id,
-                },
-                data: {
-                    up: status,
-                    updated_at: new Date(currentDate.getTime() + 60 * 60 * 1000),
-                },
-            });
-            return vote;
-    };
+            project: {
+                connect: { id: data.projectId },
+            },
+            up: data.status,
+        },
+    });
+    return vote;
+};
+export const updateVote = async (id: number, status: boolean) => {
+    const currentDate = new Date();
+    const vote = await prisma.votes.update({
+        where: {
+            id: id,
+        },
+        data: {
+            up: status,
+            updated_at: new Date(currentDate.getTime() + 60 * 60 * 1000),
+        },
+    });
+    return vote;
+};
+export const deleteVote = async (voteId: number) => {
+    const vote = await prisma.votes.delete({
+        where: {
+            id: voteId,
+        },
+    });
+    return vote;
+};
