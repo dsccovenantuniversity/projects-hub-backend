f4eccd6d2e67ed1680686d1599b5d03e6d9eacdb
daudujohn <daudujoe@gmail.com>
2024-01-29 15:10:59 +0100

    feat: add comment validator

--- src/validators/comment.ts
+++ src/validators/comment.ts
@@ -1,11 +1,27 @@
 import Joi, { ValidationResult } from 'joi';
-import { createCommentDto } from '../interfaces/comment.dto';
+import {
+    createCommentDto,
+    updateCommentByIdDto,
+} from '../interfaces/comment.dto';
+
 const createCommentSchema = Joi.object({
     userId: Joi.number().required(),
-    projectId: Joi.number().required(),
+    body: Joi.string().min(2).max(2000).required(),
 });
+
+const updateCommentSchema = Joi.object({
+    commentId: Joi.number().required(),
+    body: Joi.string().min(2).max(2000).required(),
+});
+
 export const validateNewComment = (
     data: createCommentDto,
 ): ValidationResult => {
-    return createCommentSchema.validate(data);
+    return createCommentSchema.validate(data, { abortEarly: false });
+};
+
+export const validateUpdatedComment = (
+    data: updateCommentByIdDto,
+): ValidationResult => {
+    return updateCommentSchema.validate(data, { abortEarly: false });
 };
