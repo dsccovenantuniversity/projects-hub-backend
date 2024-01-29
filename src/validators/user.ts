b2acfc337b70866d34127fed369eda1b5a975d2b
daudujohn <daudujoe@gmail.com>
2024-01-29 15:15:09 +0100

    feat: add optional attribute to user validation schema

--- src/validators/user.ts
+++ src/validators/user.ts
@@ -1,13 +1,15 @@
 import Joi, { ValidationResult } from 'joi';
-import { createUserDto } from '../interfaces/user.dto';
+import { updateUserDetailsDto } from '../interfaces/user.dto';
 
 const updateUserSchema = Joi.object({
-    first_name: Joi.string().min(2).max(255),
-    last_name: Joi.string().min(2).max(255),
-    email: Joi.string().email().max(255),
-    username: Joi.string().min(2).max(255),
+    first_name: Joi.string().min(2).max(255).optional(),
+    last_name: Joi.string().min(2).max(255).optional(),
+    email: Joi.string().email().max(255).optional(),
+    username: Joi.string().min(2).max(255).optional(),
 });
 
-export const validateUpdatedUser = (data: any): ValidationResult => {
-    return updateUserSchema.validate(data);
+export const validateUpdatedUser = (
+    data: updateUserDetailsDto,
+): ValidationResult => {
+    return updateUserSchema.validate(data, { abortEarly: false });
 };
