861b220ebc6272c0103d1945992e1d504ed58be6
daudujohn <daudujoe@gmail.com>
2024-01-29 15:16:09 +0100

    feat: update validation option

--- src/validators/vote.ts
+++ src/validators/vote.ts
@@ -1,10 +1,12 @@
 import Joi, { ValidationResult } from 'joi';
 import { createVoteDto } from '../interfaces/vote.dto';
+
 const createVoteSchema = Joi.object({
     userId: Joi.number().required(),
     projectId: Joi.number().required(),
     status: Joi.boolean().required(),
 });
+
 export const validateNewVote = (data: createVoteDto): ValidationResult => {
-    return createVoteSchema.validate(data);
+    return createVoteSchema.validate(data, { abortEarly: false });
 };
