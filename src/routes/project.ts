bb47a642b454ab4d135350ddb534780c2773db37
daudujohn <daudujoe@gmail.com>
2024-01-29 15:09:16 +0100

    feat: add validation middleware to project routes

--- src/routes/project.ts
+++ src/routes/project.ts
@@ -1,9 +1,15 @@
 import { Router } from 'express';
 import { createProjectController } from '../controller/project';
 import { asyncTryCatch } from '../middlewares/asyncTryCatch';
+import { validate } from '../middlewares/validate';
+import { validateNewProject } from '../validators/project';
 
 const router = Router();
 
-router.post('/', asyncTryCatch(createProjectController));
+router.post(
+    '/',
+    validate(validateNewProject),
+    asyncTryCatch(createProjectController),
+);
 
 export default router;
